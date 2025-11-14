99aa5029d166b6653d12ef32c724b72b27d6de82
lucasmbueno6-lab <lucas.m.bueno6@aluno.senai.br>
2025-11-14 14:28:05 +0000

    Aula Fixture

---
+++ test_mock.py
@@ -0,0 +1,20 @@
+import pytest
+import requests
+from unittest.mock import MagicMock
+
+@pytest.fixture
+def mock_response():
+    mock = MagicMock(spec=requests.Response)
+    mock.status_code = 200
+    mock.json.return_value = {'message': "Sucess"}
+    return mock
+
+def test_api_call_with_mock(mock_response):
+        response = mock_response
+        assert response.status_code == 200
+        assert response.json() == {'message':'Sucess'}
+
+def test_api_call_with_mock(mock_response):
+        response = mock_response
+        assert response.status_code == 200
+        assert response.json() == {'message':'Sucess'}
